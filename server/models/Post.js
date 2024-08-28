b91654842f0349d484c9413b889cb546dd05fe6e
HanzRod3 <rodriguezhanzel89@gmail.com>
2024-08-28 17:45:07 -0400

    fulling runing backend CRUD and reactions

---
+++ server/models/Post.js
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+const postSchema = new Schema({
+  message: {
+    type: String,
+    required: true,
+  },
+  mood: {
+    type: String,
+    enum: [
+      "happy",
+      "sad",
+      "worried",
+      "anxious",
+      "content",
+      "overjoyed",
+      "overwhelmed",
+    ], //  Mood must be one of these values
+    required: true, // The mood field is required
+  },
+  reactions: {
+    like: { type: Number, default: 0 }, //  Initialize like reactions to 0
+    love: { type: Number, default: 0 }, //  Initialize love reactions to 0
+    hug: { type: Number, default: 0 }, //  Initialize hug reactions to 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now, //  Set the createdAt field to the current date and time
+  },
+});
+export default model("Post", postSchema);
